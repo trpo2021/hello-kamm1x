24cd2083bf4f9766cb980a4aa81e0e105d4bac94
Желнеровский Иван Евгеньевич <ip015s10@bwp06.csc.local>
2021-02-10 16:12:30 +0700

    Added E

--- main.c
+++ main.c
@@ -3,11 +3,11 @@ int main()
 {
   return 0;
 }
-=======
+======
 #include <stdio.h>
 int main()
 {
-    printf("Hello World!\n");
+    printf("Hello Worlde!\n");
     return 0;
 }
 >>>>>>> 3e419fb2468144da8d35bdc614d9641a562c797a
