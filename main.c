9f95a28e145e8c9826dfa59522e853d50ed871c4
Камалов Михаил Валентинович <ip015s11@bwp06.csc.local>
2021-03-10 15:49:02 +0700

    Edit main.c

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-int main()
-{
-  return 0;
-}
-======
 #include <stdio.h>
 int main()
 {
     printf("Hello Worlde!\n");
     return 0;
 }
->>>>>>> 3e419fb2468144da8d35bdc614d9641a562c797a
+
